2e6fb243692cb907b88b89380ccd6430c1ea3db4
Chion Tang <sdspeedonion@gmail.com>
2017-06-19 01:55:29 +0800

    titan season 2 OP

--- generate_ascii_art.py
+++ generate_ascii_art.py
@@ -1,76 +1,85 @@
 from PIL import Image
+import os
+import time 
+import cv2
 
-video_length = 218
-
-ASCII_CHARS = '$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,"^`\'. '
+ASCII_CHARS = '@&B9#SGHMh352AXsri;:,. '[::-1]
+#ASCII_CHARS = '$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,"^`\'. '[::-1]
 
 def scale_image(image, new_width=100, new_height=30):
-    """Resizes an image preserving the aspect ratio.
-    """
-    (original_width, original_height) = image.size
-    aspect_ratio = original_height/float(original_width)
-    if new_height == 0:
-        new_height = int(aspect_ratio * new_width)
+  """Resizes an image preserving the aspect ratio.
+  """
+  (original_width, original_height) = image.size
+  aspect_ratio = original_height/float(original_width)
+  if new_height == 0:
+    new_height = int(aspect_ratio * new_width)
 
-    new_image = image.resize((new_width, new_height))
-    return new_image
+  new_image = image.resize((new_width, new_height))
+  return new_image
 
 def convert_to_grayscale(image):
-    return image.convert('L')
+  return image.convert('L')
 
-def map_pixels_to_ascii_chars(image, range_width=3.69):
-    """Maps each pixel to an ascii char based on the range
-    in which it lies.
+def map_pixels_to_ascii_chars(image, range_width=(255.0/(len(ASCII_CHARS) - 1))):
+  """Maps each pixel to an ascii char based on the range
+  in which it lies.
 
-    0-255 is divided into 11 ranges of 25 pixels each.
-    """
+  0-255 is divided into 11 ranges of 25 pixels each.
+  """
 
-    pixels_in_image = list(image.getdata())
-    pixels_to_chars = [ASCII_CHARS[int(pixel_value/range_width)] for pixel_value in
-            pixels_in_image]
+  pixels_in_image = list(image.getdata())
+  pixels_to_chars = [ASCII_CHARS[int(pixel_value/range_width)] for pixel_value in
+      pixels_in_image]
 
-    return "".join(pixels_to_chars)
+  return "".join(pixels_to_chars)
 
-def convert_image_to_ascii(image, new_width=100, new_height=30):
-    image = scale_image(image, new_width, new_height)
-    image = convert_to_grayscale(image)
+def convert_image_to_ascii(image, new_width=470, new_height=120):
+  image = scale_image(image, new_width, new_height)
+  image = convert_to_grayscale(image)
 
-    pixels_to_chars = map_pixels_to_ascii_chars(image)
-    len_pixels_to_chars = len(pixels_to_chars)
+  pixels_to_chars = map_pixels_to_ascii_chars(image)
+  len_pixels_to_chars = len(pixels_to_chars)
 
-    image_ascii = [pixels_to_chars[index: index + new_width] for index in
-            xrange(0, len_pixels_to_chars, new_width)]
+  image_ascii = [pixels_to_chars[index: index + new_width] for index in
+      xrange(0, len_pixels_to_chars, new_width)]
 
-    return "\n".join(image_ascii)
+  ret = "\n".join(image_ascii)
+
+  os.system('clear')
+  print(ret)
+
+  return ret
 
 def handle_image_conversion(image_filepath):
-    image = None
-    try:
-        image = Image.open(image_filepath)
-    except Exception, e:
-        print "Unable to open image file {image_filepath}.".format(image_filepath=image_filepath)
-        print e
-        return
-    image_ascii = convert_image_to_ascii(image)
-    return image_ascii
+  image = None
+  try:
+    image = Image.open(image_filepath)
+  except Exception, e:
+    print "Unable to open image file {image_filepath}.".format(image_filepath=image_filepath)
+    print e
+    return
+  image_ascii = convert_image_to_ascii(image)
+  return image_ascii
 
 if __name__=='__main__':
-    import os
-    import time 
-    import cv2
-    vidcap = cv2.VideoCapture('video.mp4')
-    time_count = 0
-    frames = []
-    while time_count <= video_length*1000:
-        print('Generating ASCII frame at ' + str(time_count))
-        vidcap.set(0, time_count)
-        success, image = vidcap.read()
-        if success:
-            cv2.imwrite('output.jpg', image)
-        frames.append(handle_image_conversion('output.jpg'))
-        time_count = time_count + 100
+  vidcap = cv2.VideoCapture('video.mp4')
+  total_frames = int(vidcap.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT))
+  fps = vidcap.get(cv2.cv.CV_CAP_PROP_FPS)
+  video_length = total_frames / fps
+  print('video_length=' + str(video_length))
 
-    f = open('play.txt', 'w')
-    f.write('SPLIT'.join(frames))
-    f.close()
+  time_count = 0
+  frames = []
+  while time_count <= video_length*1000:
+    print('Generating ASCII frame at ' + str(time_count))
+    vidcap.set(0, time_count)
+    success, image = vidcap.read()
+    if success:
+      cv2.imwrite('output.jpg', image)
+    frames.append(handle_image_conversion('output.jpg'))
+    time_count = time_count + 100
+
+  f = open('play.txt', 'w')
+  f.write('SPLIT'.join(frames))
+  f.close()
 
--- run.py
+++ run.py
@@ -1,16 +1,23 @@
 import time, os
 
 if __name__ == '__main__':
-	f = open('play.txt', 'r')
-	frame_raw = f.read()
-	frame_raw = frame_raw.replace('.', ' ')
-	f.close()
-	frames = frame_raw.split('SPLIT')
-	os.system('mplayer -vo null -vc null video.mp4 &')
-	#os.system('mplayer test.mp4 &')
-	init_time = time.time()
-	while time.time() <= init_time + 218:
-		os.system('clear')
-		print(frames[int((time.time()-init_time)*10)])
-		time.sleep(0.05)
+  f = open('play.txt', 'r')
+  frame_raw = f.read()
+  frame_raw = frame_raw.replace('.', ' ')
+  f.close()
+  frames = frame_raw.split('SPLIT')
+  os.system('mplayer -vo null -vc null video.mp4 > /dev/null 2>&1 &')
+  #os.system('mplayer test.mp4 &')
+  init_time = time.time()
+
+  import cv2
+  vidcap = cv2.VideoCapture('video.mp4')
+  total_frames = int(vidcap.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT))
+  fps = vidcap.get(cv2.cv.CV_CAP_PROP_FPS)
+  video_length = total_frames / fps
+
+  while time.time() <= init_time + video_length:
+    os.system('clear')
+    print(frames[int((time.time()-init_time)*10)])
+    time.sleep(0.05)
 
